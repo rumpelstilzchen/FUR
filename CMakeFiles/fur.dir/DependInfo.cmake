ee0d19b58ad622a67cd9eb8caefab0e1c5ed2c27
namor <namor@kira.(none)>
2010-05-28 14:07:50 +0200

    Unix Makefiles generated

---
+++ CMakeFiles/fur.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/namor/workspace/FUR/main.cxx" "/home/namor/workspace/FUR/CMakeFiles/fur.dir/main.cxx.o"
+  "/home/namor/workspace/FUR/test.cxx" "/home/namor/workspace/FUR/CMakeFiles/fur.dir/test.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ CMakeFiles/fur.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fur.dir/main.cxx.o"
+  "CMakeFiles/fur.dir/test.cxx.o"
+  "fur.pdb"
+  "fur"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
