fcf990930e4b3d17819a5676f513591bff01fc63
Harald Andre Veen <nonick37@gmail.com>
2010-05-28 14:19:48 +0200

    main.cxx geändert

--- main.cxx
+++ main.cxx
@@ -1,9 +1,35 @@
+#include "character.hxx"
 #include <iostream>
+#include "libtcod.hpp"
 
 using namespace std;
 
 int main()
 {
-  cout << "Hello, World!" << std::endl;
-  return 0;
+    TCODConsole::initRoot(80,50,"my game",false);
+    TCODMap*m = new TCODMap (100, 100);
+    m->setProperties (1,1, false, true);
+
+    Character *c = new Character();
+    c->incExperience(4);
+    int x = 5, y = 8;
+    TCODLine::init(x,y,13,4);
+    do {
+    // update cell x,y
+    } while (!TCODLine::step(&x,&y));
+
+    int endGame=0;
+
+    while ( ! endGame && ! TCODConsole::isWindowClosed() ) {
+//    ... draw on TCODConsole::root
+        TCODConsole::root->setBackgroundColor(TCODColor::desaturatedRed);
+        TCODMouse::showCursor (false);
+        TCODConsole::root->setChar(3, 4, 53);
+        TCODConsole::flush();
+
+        TCOD_key_t key=TCODConsole::waitForKeypress(true);
+//    ... update world, using key
+    }
+
+    return 0;
 }
