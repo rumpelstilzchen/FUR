8cabd826d9e792647b79d30f5ddde0aea0cdc968
Harald Andre Veen <nonick37@gmail.com>
2010-06-17 16:53:54 +0200

    Rumspielen

--- src/main.cxx
+++ src/main.cxx
@@ -6,50 +6,60 @@ using namespace std;
 
 int main()
 {
-    TCODConsole::initRoot(80,50,"FUR",false);
-    TCODMap*m = new TCODMap (100, 100);
-    //    m->setProperties (5,5,true,false);
+  int winSizeX = 80, winSizeY = 50;
+  TCODConsole::initRoot(winSizeX,winSizeY,"FUR",false);
+  int mapSizeX = 100, mapSizeY = 100;
+  TCODMap*m = new TCODMap (mapSizeX,mapSizeY);
+  //    m->setProperties (5,5,true,false);
+  for (int i=0;i<50;i++)
+    for (int j=0;j<50;j++)
+      m->setProperties(i,j,true,true);
 
-    {
-      int x = 5, y = 8;
-      TCODLine::init(x,y,13,4);
-      do {
-	m->setProperties (x,y,true,false);
-      } while (!TCODLine::step(&x,&y));
-    }
-    
-    int endGame=0;
+  int endGame=0;
 
-    //    const char *font="terminal.png";
-    TCODConsole::root->setForegroundColor(TCODColor::lightBlue);
-    TCODConsole::root->setBackgroundColor(TCODColor::black);
-    TCODMouse::showCursor (false);
-    TCODConsole::root->clear();
-    
-    int px=7,py=7;
 
-    m->computeFov(px,py);
-    while (!endGame && !TCODConsole::isWindowClosed()) {
-      for(int i=0;i<80;i++)
-	for(int j=0;j<50;j++)
-	  if(m->isInFov(i,j))
-	    TCODConsole::root->setChar(i,j,'.');
-      
-      TCODConsole::root->setChar(px,py,'@');
+  //    const char *font="terminal.png";
+  TCODConsole::root->setForegroundColor(TCODColor::lightBlue);
+  TCODConsole::root->setBackgroundColor(TCODColor::black);
+  TCODMouse::showCursor (false);
+  TCODConsole::root->clear();
 
-      TCODConsole::flush();
-      
-      TCOD_key_t key=TCODConsole::waitForKeypress(true);
-      if(key.vk==TCODK_LEFT)
-	px--;
-      if(key.vk==TCODK_RIGHT)
-	px++;
-      if(key.vk==TCODK_UP)
-	py--;
-      if(key.vk==TCODK_DOWN)
-	py++;
-      m->computeFov(px,py);
-    }
-    
-    return 0;
+  int px=7,py=7; //startpos
+
+  m->computeFov(px,py);
+  while (!endGame && !TCODConsole::isWindowClosed()) {
+    for (int i=0;i<80;i++)
+      for (int j=0;j<50;j++)
+        if (m->isInFov(i,j))  TCODConsole::root->setChar(i,j,'.');
+    //zufällig zeichnen
+    TCODConsole::root->setChar(10,15,'1');
+    TCODConsole::root->setChar(10,20,'2');
+    TCODConsole::root->setChar(10,30,'3');
+    TCODConsole::root->setChar(15,40,'4');
+
+    TCODConsole::root->setChar(px,py,'@');
+
+//test
+    //TCODConsole::root->hline(1,0, 50, TCOD_BKGND_SET);
+    //TCODConsole::root->vline(0,1, 49, TCODConsole::root->);
+    TCODConsole::root->printFrame(0,0,50,50,false,TCOD_BKGND_SET,NULL);
+    TCODConsole::root->flush();
+
+    TCOD_key_t key=TCODConsole::waitForKeypress(true);
+    if (key.vk==TCODK_LEFT)
+      px--;
+    if (key.vk==TCODK_RIGHT)
+      px++;
+    if (key.vk==TCODK_UP)
+      py--;
+    if (key.vk==TCODK_DOWN)
+      py++;
+    if (key.vk==TCODK_ESCAPE)
+      endGame=1;
+
+    m->computeFov(px,py); //compute new fov
+
+  }
+
+  return 0;
 }
