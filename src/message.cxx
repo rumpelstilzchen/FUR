51bb5d38658ca730bdc1538d2c0fe675b1210b63
Harald Andre Veen <nonick37@gmail.com>
2010-07-10 00:40:57 +0200

    msg line final methinks

---
+++ src/message.cxx
@@ -0,0 +1,41 @@
+#include "message.hpp"
+#include <string>
+
+using namespace fur;
+//fur::Message::Message(std::string& s,TCOD_bkgnd_flag_t f,const TCODColor c)
+Message::Message()
+{
+  msg="";
+}
+
+Message::Message(std::string x)
+{
+  msg=x;
+}
+
+Message::Message(std::string s,TCOD_bkgnd_flag_t f,TCODColor c)
+{
+     flag=f;
+     msg=s;
+     colour=c;
+}
+
+Message::~Message()
+{
+
+}
+
+const char*Message::c_str()
+{
+    return msg.c_str();
+}
+
+TCOD_bkgnd_flag_t Message::getFlag()
+{
+    return flag;
+}
+
+TCODColor Message::getColour()
+{
+    return colour;
+}
